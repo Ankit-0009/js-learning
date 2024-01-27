a2ddfc296826ffc89057432d0a577154631f00cb
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2024-01-27 07:38:16 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as new version
+
+//alert(3 + 3) // We are using nodejs, not browser
+
+console.log(3+3) //Readiablity of code should be high.
+
+// Datatypes --> Primitive
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => show that value has not assign
+// symbol => generally used in react for uniqueness
+
+// Datatypes --> Non Primitive
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
